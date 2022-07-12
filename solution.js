b1b4659c8ea5e0d058e4c32c7c2c3a3c81d09595
Danica Yener <ny21037@googlemail.com>
2022-07-12 16:45:06 +0200

    the last one was hard

---
+++ solution.js
@@ -0,0 +1,24 @@
+let i = 0;
+
+while (i < 15) {
+  i++;
+  console.log(i);
+}
+
+//2
+
+console.log("-----------");
+i = 1;
+sum = 0;
+while (i <= 20) {
+  sum = sum + 1;
+  i++;
+}
+console.log(sum);
+//3
+i = 1;
+do{
+    console.log("the number is", i);
+    i++;}
+     while (i <= 20) 
+    
